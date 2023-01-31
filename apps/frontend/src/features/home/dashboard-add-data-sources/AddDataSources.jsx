31c285b28d43f1d76b6ccb1bc26948ba4f3a94ca
maneesha_lakshani <maneeshalakshani9@gmail.com>
2023-02-01 01:07:37 +0530

    feat: add data sources desktop UI

---
+++ apps/frontend/src/features/home/dashboard-add-data-sources/AddDataSources.jsx
@@ -0,0 +1,73 @@
+import { createStyles, TextInput  } from "@mantine/core";
+import { DashboardAccordion } from "./DashboardAccordion";
+// import { IconSearch } from '@tabler/icons';
+
+const useStyles = createStyles((theme) => ({
+	main: {
+		background: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+		borderRadius: theme.radius.md,
+	},
+    addBox: {
+        padding: "2% 3%",
+        marginBottom: "3%"
+    },
+	label: {
+		color: theme.colorScheme === "dark" ? theme.white : theme.colors.dark,
+        fontWeight: 700,
+        fontSize: "25px",
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        padding: "2% 2.5% 2%",
+        boxShadow: "2px gray",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        background: "pink",
+	},
+    inputBox: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "3%",
+        flexDirection: "column",
+    },
+    continueBtnBox: {
+        marginTop: "3%",
+        width: "30%",
+        background: "red",
+        borderRadius: "5px",
+    },
+    continueBtn: {
+        width: "100%"
+    },
+    search:{
+        width: "50%",
+    }
+}));
+
+
+function AddDataSources() {
+   const { classes, theme } = useStyles();
+
+  return (
+    <div className={classes.main}>
+        <div className={classes.label} justify="spaceBetween">
+            <div>{window.innerWidth >= 500 ? "Add you data sources" : "A"}</div>
+            <TextInput
+                placeholder="Search"
+                // icon={<IconSearch size={16} />}
+                rightSectionWidth={90}
+                // rightSection={rightSection}
+                styles={{ rightSection: { pointerEvents: 'none' } }}
+                className={classes.search}
+            />
+        </div>
+        <div className={classes.addBox}>
+            <div className={classes.inputBox}>
+                <DashboardAccordion />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddDataSources
---
+++ apps/frontend/src/features/home/dashboard-add-data-sources/DashboardAccordion.jsx
@@ -0,0 +1,107 @@
+import { Accordion, Button, CloseButton, createStyles, Divider, Text, TextInput } from '@mantine/core';
+import { useState } from 'react';
+import { IconPlus,IconPhoto } from '@tabler/icons';
+import fbIcon from '../../../assets/facebook.png';
+import plus from '../../../assets/plus.png';
+
+const useStyle = createStyles(() => ({
+  accordionContainer: {
+    width: "80%",
+  },
+  sourceInputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  sourceInput: {
+    width: "80%",
+  },
+  sourceInputBtn: {
+    width: "18%",
+  },
+  sourcesDataContainer: {
+    paddingTop: "3%",
+  },
+  infoContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "0 20px"
+  }
+}));
+
+const sourcesTypes = [
+  {label: "Facebook Ads",},
+  {label: "Facebook Insights",},
+  {label: "Custom Data Source",},
+];
+
+const sourcesInfoData = [
+  {text: "Maneesha"},
+  {text: "Lakshani"},
+  {text: "ABCD"},
+  {text: "Maneesha Lakshani"},
+];
+
+
+export function DashboardAccordion() {
+  const { classes, theme } = useStyle()
+  const [ source, setSource ] = useState()
+  const [sourcesInfo, setSourceInfo ] = useState(sourcesInfoData)
+
+  const getColor = (color) => theme.colors[color][theme.colorScheme === 'dark' ? 5 : 7];
+
+  const addDataSources = (classes) => {
+    return sourcesTypes.map((e, i) => {
+      return (
+        <Accordion.Item value={e.label} key={i}>
+          <Accordion.Control icon={<img src={fbIcon} width={50}/>}>{e.label}</Accordion.Control>
+          <Accordion.Panel>
+            <div className={classes.sourceInputContainer}>
+              <TextInput placeholder="Enter here..." className={classes.sourceInput} onChange={(val) => setSource(val.target.value)} />
+              <Button className={classes.sourceInputBtn} onClick={addSources}>Add</Button>
+            </div>
+            <div className={classes.sourcesDataContainer}>
+              {displaySources(classes)}
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+      );
+    })
+  }
+  
+  const addSources = () => {
+    console.log(`from add source: ${source}`)
+  }
+  
+  const displaySources = (classes) => {
+    return sourcesInfo.map((e, i) => {
+      return (
+        <div>
+          <div className={classes.infoContainer}>
+            <Text c="dimmed">{e.text}</Text>
+            <CloseButton aria-label="Close modal" size={18} />
+          </div>
+          <Divider my="sm" />
+        </div>
+      );
+    });
+  }
+
+  return (
+    <Accordion 
+      defaultValue="Facebook Ads" 
+      className={classes.accordionContainer}
+      variant="filled"
+      chevronPosition="right"
+      chevron={<img src={plus} width={15} />}
+      // styles={{
+      //   item: {
+      //     border: 0,
+      //   },
+      // }}
+    >
+      {addDataSources(classes)}
+    </Accordion>
+  );
+}
