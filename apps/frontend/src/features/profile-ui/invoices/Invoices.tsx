f9d4b0609ae653a76d09b62eee51fd5378202e3d
maneesha_lakshani <maneeshalakshani9@gmail.com>
2023-02-14 11:09:44 +0530

    feat: invoices table

--- apps/frontend/src/features/profile-ui/invoices/Invoices.tsx
+++ apps/frontend/src/features/profile-ui/invoices/Invoices.tsx
@@ -1,37 +1,78 @@
-import { createStyles, Group, Table, Title } from '@mantine/core'
+import { Anchor, createStyles, Group, Table, Title } from '@mantine/core'
+import DateIcon from '../../../assets/date-icon.png'
+import DocIcon from '../../../assets/doc-icon.png'
+import DollarIcon from '../../../assets/dollar-icon.png'
+import DownloadIcon from '../../../assets/download-icon.png'
+import MenuIcon from '../../../assets/menu-icon.png'
+import SelectIcon from '../../../assets/select-icon.png'
 
 const useStyle = createStyles((theme) => ({
   table: {
     marginTop: "7%",
-  }
+  },
+  tableRow: {
+    textAlign: "center",
+  },
 }))
 
 function Invoices() {
   const { classes, theme } = useStyle()
 
   const elements = [
-    { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
-    { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-    { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-    { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-    { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+    { iNo: 6, date: "12/12/2022", payment: 40, status: 'Pending' },
+    { iNo: 7, date: "12/12/2022", payment: 300, status: 'Paid' },
+    { iNo: 39, date: "12/12/2022", payment: 40.33, status: 'Paid' },
+    { iNo: 56, date: "12/12/2022", payment: 1000, status: 'Paid' },
+    { iNo: 58, date: "12/12/2022", payment: 10000, status: 'Pending' },
   ];
 
   const ths = (
     <tr>
-      <th>Element position</th>
-      <th>Element name</th>
-      <th>Symbol</th>
-      <th>Atomic mass</th>
+      <th>
+        <Group position="center" spacing="sm">
+          <img src={MenuIcon} width={15} />
+          <div>Invoice Number</div>
+        </Group>
+      </th>
+      <th>
+        <Group position="center" spacing="sm">
+          <img src={DateIcon} width={20} />
+          <div>Date</div>
+        </Group>
+      </th>
+      <th>
+        <Group position="center" spacing="sm">
+          <img src={DollarIcon} width={20} />
+          <div>Payment</div>
+        </Group>
+      </th>
+      <th>
+        <Group position="center" spacing="sm">
+          <img src={SelectIcon} width={20} />
+          <div>Status</div>
+        </Group>
+      </th>
+      <th>
+        <Group position="center" spacing="sm">
+          <img src={DocIcon} width={20} />
+          <div>Incoice</div>
+        </Group>
+      </th>
     </tr>
   );
 
   const rows = elements.map((element) => (
-    <tr key={element.name}>
-      <td>{element.position}</td>
-      <td>{element.name}</td>
-      <td>{element.symbol}</td>
-      <td>{element.mass}</td>
+    <tr key={element.iNo} className={classes.tableRow}>
+      <td>{element.iNo}</td>
+      <td>{element.date}</td>
+      <td>{element.payment}</td>
+      <td>{element.status == "Pending" ? "Pending" : "Paid"}</td>
+      <td>
+        <Group position="center" spacing="md">
+          <Anchor underline>View</Anchor>
+          <img src={DownloadIcon} width={10} />
+        </Group>
+      </td>
     </tr>
   ));
 
